81d5ac40d2d7b1cec9818d1d94a66cd7be9bf16f
Dhanush Suvarna <suvarnadhanush2000@gmail.com>
2025-03-03 00:19:50 +0530

    feat: add initial extension files and Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        background: './src/background.ts',
+        content: './src/content.ts',
+      },
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: 'chunks/[name].js',
+        assetFileNames: 'assets/[name].[ext]',
+      }
+    }
+  }
 })
